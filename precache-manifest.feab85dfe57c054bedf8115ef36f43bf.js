5e6919d02c1247b27dfe0971acbab48e04000ff4
RaphaAmericano <raphael@raphaelamericano.com.br>
2022-06-02 13:31:35 -0300

    Updates

---
+++ precache-manifest.feab85dfe57c054bedf8115ef36f43bf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f81529045929ffe4bb629b912bca9a40",
+    "url": "/react-dnd-example-many-tables/index.html"
+  },
+  {
+    "revision": "a9347cdaea836e4bf43a",
+    "url": "/react-dnd-example-many-tables/static/js/2.b6512189.chunk.js"
+  },
+  {
+    "revision": "98c725c1ccd3bb3f4708",
+    "url": "/react-dnd-example-many-tables/static/js/main.2de87de1.chunk.js"
+  },
+  {
+    "revision": "3f52f6ed6d0b7590c652",
+    "url": "/react-dnd-example-many-tables/static/js/runtime~main.b07dec2b.js"
+  }
+]);
